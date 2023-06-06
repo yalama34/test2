466cdc114af9b244159a9bcc63dd6ac3691c11f4
yalama34 <127877015+yalama34@users.noreply.github.com>
2023-06-06 19:46:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-function setup() {
-    createCanvas(windowWidth,windowHeight)
-    
-    create()
-  }
-  const rand = () => (random() - .5) * 2;
-  
-  const create = () => {
-    var p = new PoissonDiskSampling({
-      shape: [width, height],
-      minDistance: random(13,20),
-      maxDistance: random(45,55),
-      tries: 10
-    });
-    var points = p.fill();
-    
-    stroke("hsl(0,90%,20%)");
-    
-    points.forEach((point,i) => {
-    fill(`hsl(0,90%,${40 * (i/points.length)}%)`);
-      push()
-      
-      translate(point)
-      
-      let rounds = floor(random()*3) + 3;
-      
-      for(var r = rounds; r > 0; r--) {
-        const x = point[0] + rand();
-        const y = point[1] + rand();
-        ellipse(x,y,r*15,r*15)
-      }
-      
-      pop()
-    })
-  }
-  
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    create()
-  }
-  function mouseClicked () { create() }
-  function touchEnded () { create() }
+const BUTTON = document.querySelector("button");
+const SYNC = document.querySelector("#sync")
+
+const TOGGLE = () => {
+  const IS_PRESSED = BUTTON.matches("[aria-pressed=true]");
+  if (SYNC.checked)
+    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
+  BUTTON.setAttribute("aria-pressed", IS_PRESSED ? false : true);
+};
+
+BUTTON.addEventListener("click", TOGGLE);
